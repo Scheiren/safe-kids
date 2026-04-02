3cf55e47aa454845aadb3dc410f487592d84a9d1
Nguyen Vu Hoang Anh <nguyenvuhoanganh1309@gmail.com>
2026-04-02 15:21:28 +0700

    Include video files in Vite assets
    Add video file types to assets include configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.mp4', '**/*.m4a'], // Thêm dòng này để Vite hiểu video là tài nguyên
 })
